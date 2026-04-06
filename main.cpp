487a59affc27b8be0ed13e6d9fb5a85f8b8bd7f7
yuvrajsidhu08 <454009@bsd48.org>
2026-04-05 22:46:25 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <cstdlib>
 using namespace std;
 
+// Node structure for BST
 struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
+// Create a new node
 Node* create(int v) {
     Node* n = new Node;
     n->data = v;
@@ -16,6 +18,7 @@ Node* create(int v) {
     return n;
 }
 
+// Insert a value into the BST
 Node* insert(Node* root, int v) {
     if (!root) return create(v);
     if (v < root->data) root->left = insert(root->left, v);
@@ -23,11 +26,13 @@ Node* insert(Node* root, int v) {
     return root;
 }
 
+// Find the minimum node in a subtree
 Node* findMin(Node* root) {
     while (root->left) root = root->left;
     return root;
 }
 
+// Remove a node with a given value
 Node* removeNode(Node* root, int v) {
     if (!root) return NULL;
 
@@ -45,12 +50,14 @@ Node* removeNode(Node* root, int v) {
     return root;
 }
 
+// Search for a value in the BST
 bool search(Node* root, int v) {
     if (!root) return false;
     if (root->data == v) return true;
     return v < root->data ? search(root->left, v) : search(root->right, v);
 }
 
+// Print the BST sideways
 void printTree(Node* root, int space) {
     if (!root) return;
     space += 5;
@@ -60,16 +67,17 @@ void printTree(Node* root, int space) {
     printTree(root->left, space);
 }
 
+// Add numbers from a string to the BST
 void addNumbers(Node*& root, char input[]) {
     char* token = strtok(input, " ");
     while (token) {
         int num = atoi(token);
-        if (num >= 1 && num <= 999)
-            root = insert(root, num);
+        if (num >= 1 && num <= 999) root = insert(root, num);
         token = strtok(NULL, " ");
     }
 }
 
+// Main program loop
 int main() {
     Node* root = NULL;
     char command[20];
@@ -86,6 +94,7 @@ int main() {
         cout << "\nEnter command: ";
         cin >> command;
 
+        // Add numbers to the BST
         if (strcmp(command, "add") == 0) {
             cin.ignore();
             char line[200];
@@ -93,25 +102,30 @@ int main() {
             cin.getline(line, 200);
             addNumbers(root, line);
         }
+        // Remove a number from the BST
         else if (strcmp(command, "remove") == 0) {
             int x;
             cout << "Number to remove: ";
             cin >> x;
             root = removeNode(root, x);
         }
+        // Search for a number in the BST
         else if (strcmp(command, "search") == 0) {
             int x;
             cout << "Number to search: ";
             cin >> x;
             cout << (search(root, x) ? "Found\n" : "Not found\n");
         }
+        // Print the BST
         else if (strcmp(command, "print") == 0) {
             cout << "\nTree:\n";
             printTree(root, 0);
         }
+        // Quit the program
         else if (strcmp(command, "quit") == 0) {
             break;
         }
+        // Handle invalid commands
         else {
             cout << "Invalid command.\n";
         }
